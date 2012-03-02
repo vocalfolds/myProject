d3224d8ca4dd6c5cfe8934965825c6058e831c37
vocalfolds <vocalfolds@gmail.com>
2012-03-02 00:01:47 -0600

    part 3 finished

---
+++ polls/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls.defaults import patterns, include, url
+
+urlpatterns = patterns('polls.views',
+    # Examples:
+    # url(r'^$', 'myproject.views.home', name='home'),
+    # url(r'^myproject/', include('myproject.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^$', 'index'),
+    url(r'^(?P<poll_id>\d+)/$', 'detail'),
+    url(r'^(?P<poll_id>\d+)/results/$', 'results'),
+    url(r'^(?P<poll_id>\d+)/vote/$', 'vote'),
+)
