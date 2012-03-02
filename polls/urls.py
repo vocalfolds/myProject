05b914ad3ad326cb7f298c5deec28432f0d7a2a2
vocalfolds <vocalfolds@gmail.com>
2012-03-02 01:07:35 -0600

    tutorial finished

--- polls/urls.py
+++ polls/urls.py
@@ -1,16 +1,22 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.defaults import *
+from django.views.generic import DetailView, ListView
+from polls.models import Poll
 
-urlpatterns = patterns('polls.views',
-    # Examples:
-    # url(r'^$', 'myproject.views.home', name='home'),
-    # url(r'^myproject/', include('myproject.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^$', 'index'),
-    url(r'^(?P<poll_id>\d+)/$', 'detail'),
-    url(r'^(?P<poll_id>\d+)/results/$', 'results'),
-    url(r'^(?P<poll_id>\d+)/vote/$', 'vote'),
+urlpatterns = patterns('',
+    url(r'^$',
+        ListView.as_view(
+            queryset=Poll.objects.order_by('-pub_date')[:5],
+            context_object_name='latest_poll_list',
+            template_name='polls/index.html')),
+    url(r'^(?P<pk>\d+)/$',
+        DetailView.as_view(
+            model=Poll,
+            template_name='polls/detail.html')),
+    url(r'^(?P<pk>\d+)/results/$',
+        DetailView.as_view(
+            model=Poll,
+            template_name='polls/results.html'),
+        name='poll_results'),
+    url(r'^(?P<poll_id>\d+)/vote/$','polls.views.vote'),
 )
+         
